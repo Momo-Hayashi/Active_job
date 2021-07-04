9c991338db07a2492c2422f7acb9892df557d836
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-07-04 19:27:16 +0900

    Create: Active Job with Delayed Job

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2021_07_04_101700) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
